84f4b8171cf79bd1dd40ff5f3f714e71c9253acd
Brian Toon <brian.elliot.toon@gmail.com>
2025-03-07 11:04:38 -0700

    routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
-import './App.css'
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 function App() {
 
-
   return (
-    <>
-     
-    </>
+    <BrowserRouter>
+    {/* navigation here */}
+      <Routes>
+        <Route path="/" element={<p>Log in</p>} />
+        <Route path="/about" element={<p>About</p>} />
+        <Route path="/posts" element={<p>All Posts</p>} />
+        <Route path="/posts/create" element={<p>Create new post</p>} />
+        <Route path="/posts/:id" element={<p>Post details</p>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
