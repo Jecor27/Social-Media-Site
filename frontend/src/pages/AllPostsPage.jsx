fb22f9eb6b57c151f4327e78eed52a98ba1b622d
Jesus <jjcorrales76@gmail.com>
2025-03-07 13:57:31 -0700

    get all posts

--- frontend/src/pages/AllPostsPage.jsx
+++ frontend/src/pages/AllPostsPage.jsx
@@ -1,35 +1,67 @@
-import React from 'react'
-import BlogPost from '../components/BlogPost'
+// import React from 'react'
+// import BlogPost from '../components/BlogPost'
 
-const dummyPosts = [
-  {
-    id: 1,
-    title: 'My cool new blog',
-    content: 'Blah blah blah blah',
-    author: 'Bob'
-  },
-  {
-    id: 2,
-    title: 'All you need to know about Jesus',
-    content: 'Blah blah blah blah',
-    author: 'Jesus'
-  },  
-  {
-    id: 3,
-    title: 'Jamar knows best!',
-    content: 'Blah blah blah blah',
-    author: 'Jamar'
-  },
-]
+// const dummyPosts = [
+//   {
+//     id: 1,
+//     title: 'My cool new blog',
+//     content: 'Blah blah blah blah',
+//     author: 'Bob'
+//   },
+//   {
+//     id: 2,
+//     title: 'All you need to know about Jesus',
+//     content: 'Blah blah blah blah',
+//     author: 'Jesus'
+//   },
+//   {
+//     id: 3,
+//     title: 'Jamar knows best!',
+//     content: 'Blah blah blah blah',
+//     author: 'Jamar'
+//   },
+// ]
 
-export default function AllPosts() {
+// export default function AllPosts() {
+//   return (
+//     <div>
+//       <h2>All Posts</h2>
+//       <p>hello</p>
+//       {dummyPosts.map(post => (
+//         <BlogPost post={post} key={post.id} />
+//       ))}
+//     </div>
+//   )
+// }
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import BlogPost from "../components/BlogPost";
+
+function AllPosts() {
+  const [posts, setPosts] = useState([]);
+
+  async function getPosts() {
+    try {
+      const response = await axios.get("http://localhost:8080/posts");
+      console.log(response);
+      setPosts(response.data);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
   return (
     <div>
       <h2>All Posts</h2>
-      <p>hello</p>
-      {dummyPosts.map(post => (
-        <BlogPost post={post} key={post.id} />
+      {posts.map((post, index) => (
+        <BlogPost key={index} post={post} />
       ))}
     </div>
-  )
+  );
 }
+export default AllPosts;
