7b78c46c192ce1851b93a2ef6901f423ad38539e
Jesus <jjcorrales76@gmail.com>
2025-03-07 13:58:38 -0700

    get all posts

--- frontend/src/pages/AllPostsPage.jsx
+++ frontend/src/pages/AllPostsPage.jsx
@@ -1,39 +1,3 @@
-// import React from 'react'
-// import BlogPost from '../components/BlogPost'
-
-// const dummyPosts = [
-//   {
-//     id: 1,
-//     title: 'My cool new blog',
-//     content: 'Blah blah blah blah',
-//     author: 'Bob'
-//   },
-//   {
-//     id: 2,
-//     title: 'All you need to know about Jesus',
-//     content: 'Blah blah blah blah',
-//     author: 'Jesus'
-//   },
-//   {
-//     id: 3,
-//     title: 'Jamar knows best!',
-//     content: 'Blah blah blah blah',
-//     author: 'Jamar'
-//   },
-// ]
-
-// export default function AllPosts() {
-//   return (
-//     <div>
-//       <h2>All Posts</h2>
-//       <p>hello</p>
-//       {dummyPosts.map(post => (
-//         <BlogPost post={post} key={post.id} />
-//       ))}
-//     </div>
-//   )
-// }
-
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import BlogPost from "../components/BlogPost";
